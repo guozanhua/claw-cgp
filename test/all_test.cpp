5e0acc02ee227c0fb4b93cd64103e14ab104653d
Sheppard Y <ysf1026@gmail.com>
2013-07-14 21:27:08 +0800

    CMakeList and gtest lib

---
+++ test/all_test.cpp
@@ -0,0 +1,13 @@
+//Author: sheppard(ysf1026@gmail.com) 2013-07-14
+//      Desc:
+
+#include <iostream>
+#include <gtest/gtest.h>
+
+int main(int argc, char** argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    RUN_ALL_TESTS();
+    system("pause");
+    return 0;
+}
