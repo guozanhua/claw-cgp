13efd3db9c586666fd5cc5663430b158563aba74
Sheppard Y <ysf1026@gmail.com>
2013-11-11 18:59:11 +0800

    init logic server

---
+++ logic/logic.go
@@ -0,0 +1,14 @@
+// Author: sheppard(ysf1026@gmail.com) 2013-11-07
+
+package main
+
+import (
+	"fmt"
+	"github.com/yangsf5/claw-cgp/logic/player"
+)
+
+func main() {
+	fmt.Println("Logic starts ...")
+
+	fmt.Println("Logic stops ...")
+}
