6c18b685ed12adf41bc5de7de1e7f306f29b63fe
Sheppard Y <ysf1026@gmail.com>
2013-11-13 22:54:50 +0800

    define proto ping and pong between client and logic; test proto ping

--- logic/logic.go
+++ logic/logic.go
@@ -4,11 +4,25 @@ package main
 
 import (
 	"fmt"
-	"github.com/yangsf5/claw-cgp/logic/player"
+	pb "code.google.com/p/goprotobuf/proto"
+	//"github.com/yangsf5/claw-cgp/logic/player"
+	. "github.com/yangsf5/claw-cgp/proto"
 )
 
 func main() {
 	fmt.Println("Logic starts ...")
+
+	ping := &CLPing{
+		Time: pb.Int32(12),
+	}
+
+	data, _ := pb.Marshal(ping)
+
+	ping2 := new(CLPing)
+	pb.Unmarshal(data, ping2)
+
+	fmt.Println(ping2)
+
 
 	fmt.Println("Logic stops ...")
 }
