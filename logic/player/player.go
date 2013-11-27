3e9f75fc073824c428744f82feae21dc7944ba0b
Sheppard Y <ysf1026@gmail.com>
2013-11-27 21:05:22 +0800

    logic player add send and recv, etc.

--- logic/player/player.go
+++ logic/player/player.go
@@ -3,14 +3,74 @@
 package player
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"net"
+	pb "code.google.com/p/goprotobuf/proto"
+	myNet "github.com/yangsf5/claw-cgp/logic/net"
 )
 
 type Player struct {
 	Conn net.Conn
+	Disconnected bool
 }
 
 func (p *Player) Start() {
 	fmt.Println("Player start")
+	p.Disconnected = false
+	go p.Tick()
+}
+
+func (p *Player) Stop() {
+	p.Disconnected = true
+}
+
+func (p *Player) Tick() {
+	for ; !p.Disconnected; {
+		msg := p.Recv()
+		pbMsg, err := myNet.Decode(msg)
+		if err != nil {
+			fmt.Println("Message decode error", err)
+		}
+		fmt.Println(pbMsg)
+	}
+	defer p.Conn.Close()
+	fmt.Println("Client disconneted")
+}
+
+func (p *Player) Send(msg pb.Message) {
+	msgBuf, err := myNet.Encode(msg)
+	p.checkNetError(err)
+	_, err = p.Conn.Write(msgBuf)
+	p.checkNetError(err)
+}
+
+func (p *Player) Recv() []byte {
+	// Read total length
+	var totalLen uint16
+	var totalLenBuf [2]byte
+	_, err := p.Conn.Read(totalLenBuf[:])
+	p.checkNetError(err)
+	var totalLenBuffer *bytes.Buffer = bytes.NewBuffer(totalLenBuf[:])
+	binary.Read(totalLenBuffer, binary.BigEndian, &totalLen)
+
+	// Read the rest of message
+	restBuf := make([]byte, totalLen)
+	_, err = p.Conn.Read(restBuf[:])
+	p.checkNetError(err)
+
+	msg := new(bytes.Buffer)
+	msg.Write(totalLenBuf[:])
+	msg.Write(restBuf[:])
+	fmt.Println(msg)
+
+	return msg.Bytes()
+}
+
+func (p *Player) checkNetError(err error) {
+	if err != nil {
+		fmt.Println("Net error:", err.Error())
+		p.Disconnected = true
+	}
 }
