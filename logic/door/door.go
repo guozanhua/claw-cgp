06bc2a9c4ad81dcc1b74bb783c39c99368a61325
Sheppard Y <ysf1026@gmail.com>
2013-12-13 19:21:19 +0800

    door for web

---
+++ logic/door/door.go
@@ -0,0 +1,19 @@
+// Author: sheppard(ysf1026@gmail.com) 2013-12-13
+
+package door
+
+import (
+	"net"
+	"github.com/yangsf5/claw-cgp/logic/player"
+)
+
+func init() {
+}
+
+func Login(conn net.Conn) {
+	client := &player.Player {
+		Conn: conn,
+	}
+	go client.Start()
+}
+
