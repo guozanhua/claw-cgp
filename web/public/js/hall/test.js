a0b6f6a800cf5e27dc132c0ee65296d2c9533446
Sheppard Y <ysf1026@gmail.com>
2014-01-05 22:42:09 +0800

    showing messages in panel is ok

--- web/public/js/hall/test.js
+++ web/public/js/hall/test.js
@@ -3,6 +3,7 @@
  */
 
 var socket;
+var msgs = [];
 
 $(document).ready(function() {
   connect();
@@ -11,25 +12,34 @@ $(document).ready(function() {
   btn_test.text('haha');
   btn_test.click(function() {
     var content = btn_test.text(); 
-    updatePanel(content);
     socket.send(content);
     return false;
   });
 });
-
-function updatePanel(msg) {
-  $('#panel-body').text(msg);
-}
 
 function connect() {
   var userName = $('#userName').text();
   socket = new WebSocket('ws://'+window.location.host+'/hall/hall/socket?user=' + userName);
 
   socket.onmessage = function(event) {
-    console.log(event);
+    addToPanel(event.data);
+    refreshPanel();
   };
 
   socket.onclose = function() {
     console.log('disconnected');
   };
 };
+
+function addToPanel(msg) {
+  msgs.push(msg);
+};
+
+function refreshPanel() {
+  var panel = $('#panel-body');
+  panel.empty();
+  _.each(msgs, function(item) {
+    panel.append(item).append('<br>');
+  });
+}
+
